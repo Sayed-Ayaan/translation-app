acb1cbb327e3b24b975e6fac1f30b2cec81e379f
SSashinn <sayedayaan226@gmail.com>
2025-07-14 19:22:03 +0530

    send audio to backend

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
 import eventlet
+eventlet.monkey_patch()
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 import eventlet.wsgi
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True, max_http_buffer_size=2 * 1024 * 1024 + (10 * 1024))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @socketio.on('connect') 
 def on_connect():
-    print("Client connected")
+  print(f'Client Connected: {request.sid}')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print(f'Client disconnected: {request.sid}')
+    
+@socketio.on('audio')
+def handle_audio(data):
+  audio = data.get("audio")
+  code = data.get("code")
+  print(code)
+  print('Starting audio transcribing')
+  with open('./test.wav', 'wb') as f:
+      f.write(audio)
+  print('Finish audio transcribing')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+  socketio.run(app, debug=False, use_reloader=False, host='0.0.0.0', port=5000)
--- static/index.js
+++ static/index.js
@@ -1,43 +1,71 @@
 const socket = io('http://localhost:5000');
 const start = document.getElementById('startBtn');
 const stop = document.getElementById('stopBtn');
-const audioPlayer = document.getElementById('audioPlayer');
+const language = document.getElementById('language');
 
 socket.on('connect', () => {
-    console.log('Connected to backend')
+  console.log('Connected to backend')
 });
+
+socket.on('disconnect', (reason) => {
+  console.log('Disconnected:', reason);
+});
+
+const languageCodes = [
+  "afr", "amh", "arb", "ary", "arz", "asm", "ast", "azj", "bel", "ben",
+  "bos", "bul", "cat", "ceb", "ces", "ckb", "cmn", "cmn_Hant", "cym", "dan",
+  "deu", "ell", "eng", "est", "eus", "fin", "fra", "fuv", "gaz", "gle",
+  "glg", "guj", "heb", "hin", "hrv", "hun", "hye", "ibo", "ind", "isl",
+  "ita", "jav", "jpn", "kam", "kan", "kat", "kaz", "kea", "khk", "khm",
+  "kir", "kor", "lao", "lit", "ltz", "lug", "luo", "lvs", "mai", "mal",
+  "mar", "mkd", "mlt", "mni", "mya", "nld", "nno", "nob", "npi", "nya",
+  "oci", "ory", "pan", "pbt", "pes", "pol", "por", "ron", "rus", "slk",
+  "slv", "sna", "snd", "som", "spa", "srp", "swe", "swh", "tam", "tel",
+  "tgk", "tgl", "tha", "tur", "ukr", "urd", "uzn", "vie", "xho", "yor",
+  "yue", "zlm", "zsm", "zul"
+];
 
 let mediaRecorder;
 let chunks = [];
 
 // Start recording the audio
-start.addEventListener('click', () => {
-    navigator.mediaDevices.getUserMedia({audio:true})
+start.addEventListener('click', (event) => {
+  event.preventDefault();
+  navigator.mediaDevices.getUserMedia({ audio: true })
     .then((stream) => {
-        mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder = new MediaRecorder(stream);
 
-        mediaRecorder.ondataavailable = (event) => {
-            chunks.push(event.data);
-        };
-    mediaRecorder.onstop = () => {
-        const blob = new Blob(chunks, {type: 'audio/wav'});
+      mediaRecorder.ondataavailable = (event) => {
+        chunks.push(event.data);
+      };
+
+      mediaRecorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'audio/wav' });
         chunks = []
-    };
-    mediaRecorder.start();
-    start.disabled = true;
-    stop.disabled = false;
+        stream.getTracks().forEach(track => track.stop());
+        if (!languageCodes.includes(language.value)) {
+          alert('DO NOT CHANGE THE LANGUAGE CODE.');
+        } else if (blob.size >= 2 * 1024 * 1024) {
+          alert('Do not exceed 2MB');
+        } else {
+          socket.emit('audio', { audio: blob, code: language.value });
+        }
+      };
+      mediaRecorder.start();
+      start.disabled = true;
+      stop.disabled = false;
     })
     .catch((err) => {
-        console.error(`${err.name}: ${err.message}`)
+      console.error(`${err.name}: ${err.message}`)
     });
 });
 
 // Stop recording the audio
-stopBtn.addEventListener('click', () => {
-  if (mediaRecorder && mediaRecorder.state !== "inactive") {
+stop.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (mediaRecorder && mediaRecorder.state !== 'inactive') {
     mediaRecorder.stop();
-    // Disable the stop button and enable the start button for the next recording
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
+    start.disabled = false;
+    stop.disabled = true;
   }
 });
